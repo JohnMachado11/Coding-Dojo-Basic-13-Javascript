de343989037c88fc2bf803c6dfb17b56e74868b3
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2020-11-10 20:53:41 -0600

    Update SwapValues.js

--- SwapValues.js
+++ SwapValues.js
@@ -2,17 +2,10 @@
 // The default minimum length of the array is 2. (e.g. [1,5,10,-2] will become [-2,5,10,1]).
 
 function swap(arr) {
-    var value_store = arr[0];
-    var arrnew = [];
-    for (var i = 0; i < arr.length; i++){
-        if (arr[i] == arr[0]){
-            arr[0] = arr[arr.length - 1];
-        }
-        break;
-    }
-    arr[arr.length -1] = value_store;
-    arrnew = arr;
-    return arrnew; 
+    var storage = arr[0];
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length -1] = storage;
+    return arr; 
 }
 
 x = swap([4,3,8,9]); // [9,3,8,4]
